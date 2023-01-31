586d74cf720328d6e2b8c8bdeeda8faae526ace3
wilhasenac <wilhaprogramadorweb@gmail.com>
2023-01-31 20:41:46 -0300

    Adiciona novo atributo
    Foi adicionado um atributo
    novo para que seja possível a venda em quantidade

--- exemplo.py
+++ exemplo.py
@@ -1,7 +1,8 @@
 class Produto():
-    def __init__(self, nome, preco):
+    def __init__(self, nome, preco,unidade):
         self.nome = nome
         self.preco = preco
+        self.unidade = unidade
 
 
 quantidade = int(input('Informe a quantidade'))
