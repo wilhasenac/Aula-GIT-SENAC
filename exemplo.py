47c036862b6c749f8dc1c935676de8a3b621838b
wilhasenac <wilhaprogramadorweb@gmail.com>
2023-01-31 20:36:23 -0300

    Primeiro commit

---
+++ exemplo.py
@@ -0,0 +1,9 @@
+class Produto():
+    def __init__(self, nome, preco):
+        self.nome = nome
+        self.preco = preco
+
+
+quantidade = int(input('Informe a quantidade'))
+print(quantidade)
+        
